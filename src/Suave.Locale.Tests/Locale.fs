782043c4efc678432dddef71c9d70682e735b912
Henrik Feldt <henrik@haf.se>
2015-07-23 10:07:48 +0200

    better test

--- src/Suave.Locale.Tests/Locale.fs
+++ src/Suave.Locale.Tests/Locale.fs
@@ -18,18 +18,23 @@ let range =
     testList "generalise" [
       testCase "en-GB" <| fun _ ->
         Assert.Equal("next up", Range ["en"], Range.generalise (Range ["en"; "GB"]))
+
       testCase "en" <| fun _ ->
         Assert.Equal("next up", Range ["en"], Range.generalise (Range ["en"]))
+
       testCase "en-GB-XPrivate" <| fun _ ->
         Assert.Equal("next up", Range ["en"; "GB"], Range.generalise (Range ["en"; "GB"; "XPrivate"]))
       ]
     testList "checkParent" [
       testCase "en, en => Range [ en ]" <| fun _ ->
         Assert.Equal("", Choice1Of2 (Range ["en"]), Range.checkParent (Range ["en"]) (Range ["en"]))
+
       testCase "en, en-GB => Choice1Of2 $ Range [ en ]" <| fun _ ->
         Assert.Equal("", Choice1Of2 (Range ["en"]), Range.checkParent (Range ["en"]) (Range ["en"; "GB"]))
+
       testCase "en-GB, en => Choice2Of2 ()" <| fun _ ->
         Assert.Equal("", Choice2Of2 (), Range.checkParent (Range ["en"; "GB"]) (Range ["en"]))
+
       testCase "en, Any => Range [ en ]" <| fun _ ->
         Assert.Equal("", Choice1Of2 (Range ["en"]), Range.checkParent (Range ["en"]) (Range ["en"]))
       ]
@@ -40,15 +45,15 @@ let emptyData r =
     [r],
     """{"misc":{"title":"Häftig titel"}}""" |> Json.parse |> Json.deserialize)
 
-let wonkyData = """{"locales":["sv-SE"],"messages":{"misc":{"title":"Awesome Title"}}}"""
+let wonkyMsgs = """{"locales":["en"],"messages":{"misc":{"title":"Awesome Title"}}}"""
 
 [<Tests>]
 let intlData =
   testList "intl data" [
     testCase "from json" <| fun _ ->
       let rec find k (Messages kvs) = List.find (fun (key, value) -> key = k) kvs |> snd
-      let intl : IntlData = Json.parse wonkyData |> Json.deserialize
-      Assert.Equal("locale", [ Range ["sv"; "SE"] ], intl.locales)
+      let intl : IntlData = Json.parse wonkyMsgs |> Json.deserialize
+      Assert.Equal("locale", [ Range ["en"] ], intl.locales)
       Assert.Equal("title", "Awesome Title", find ["misc";"title"] intl.messages)
     ]
 
@@ -65,8 +70,10 @@ let negotiate =
     testList "findParent" [
       testCase "(Source 'en') Any => Choice1Of2 'en'" <| fun _ ->
         Assert.Equal("", Choice1Of2 (emptyData (Range ["en"])), Negotiate.findParent en Any)
+
       testCase "(Source 'en-GB') 'en' => Choice2Of2 ()" <| fun _ ->
         Assert.Equal("", Choice2Of2 (), Negotiate.findParent enGB (Range ["en"]))
+
       testCase "(Source 'en-GB') 'en-GB' => Choice2Of2 ()" <| fun _ ->
         Assert.Equal("",
                      Choice1Of2 (emptyData (Range ["en"; "GB"])),
@@ -81,18 +88,18 @@ let http =
   let neg =
     Negotiate.negotiate
       [ ReqSources.parseAcceptable ]
-      [ LangSources.fromJson wonkyData
-        function _ -> Choice1Of2 (emptyData (Range ["sv"; "SE"]))
-      ]
+      [ LangSources.fromJson wonkyMsgs
+        function _ -> Choice1Of2 (emptyData (Range ["sv"; "FI"])) ]
+      
     |> Negotiate.assumeSource
-
 
   let clientNeg header =
     let ctx = runWith defaultConfig (Http.app "/intl" neg)
+
     try
       use resp =
         Client.createRequest Client.Get (Uri "http://127.0.0.1:8083/intl")
-        |> Client.withHeader (Client.RequestHeader.AcceptLanguage "ro")
+        |> Client.withHeader (Client.RequestHeader.AcceptLanguage header)
         |> Client.getResponse
         |> Async.RunSynchronously
       
@@ -111,5 +118,5 @@ let http =
   testList "Http" [
     testCase "negotiate" <| fun _ ->
       let data = clientNeg "ro, en, sv"
-      Assert.Equal("locales", [ Range [ "sv"; "SE" ] ], data.locales)
+      Assert.Equal("locales", [ Range [ "sv"; "FI" ] ], data.locales)
     ]
